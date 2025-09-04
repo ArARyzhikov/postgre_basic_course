4db835cd2275e0b2a86690919212d81cf8c534be
Artem Ryzhikov <Artem_Ryzhikov@rgs.ru>
2025-09-04 14:23:05 +0300

    добавлены изменения

---
+++ Day00/day00_ex05.sql
@@ -0,0 +1,5 @@
+SELECT
+    (SELECT name FROM person AS pr WHERE pr.id = o.person_id) AS person_name
+FROM person_order AS o
+WHERE (menu_id = 13 or  menu_id = 14 or menu_id = 18)
+  AND order_date = '2022-01-07';
